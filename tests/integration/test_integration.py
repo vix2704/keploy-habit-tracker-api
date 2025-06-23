d9a8546773a6ef4817d372b5d7b44fce1dcdd969
vijaya mahato <vijayamahato27@gmail.com>
2025-06-24 02:16:22 +0530

    Add test suite and update README with coverage

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -12,3 +12,18 @@ def test_create_and_read_habit_integration():
             found = True
             break
     assert found
+
+from backend.crud import create_habit, get_all_habits
+from backend.models import Habit
+
+def test_create_and_read_habit_integration():
+    test_habit = Habit(id="test123", name="Water Plants", description="Every morning", is_complete=False)
+    create_habit(test_habit)
+    habits = get_all_habits()
+
+    found = False
+    for h in habits:
+        if h["name"] == "Water Plants":
+            found = True
+            break
+    assert found
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from fastapi.testclient import TestClient
 from backend.main import app
 
@@ -7,3 +8,14 @@ def test_read_all_habits():
     response = client.get("/habits/")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
+=======
+from fastapi.testclient import TestClient
+from backend.main import app
+
+client = TestClient(app)
+
+def test_read_all_habits():
+    response = client.get("/habits/")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+>>>>>>> 240859e (Add test suite and update README with coverage)
